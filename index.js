830afa07d306f46930a41004f67e9b7bf8931f01
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-11 01:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,4 @@ function happyHolidays(){
   return("Happy holidays!");
 }
 
-function happyHolidaysTo (nameInput){
-  var name = nameInput;
-  return("Happy holidays, ${name} !");
-}
-
-console.log(happyHolidaysTo("Karen"));
+function happyHolidaysTo()
